2960c1138e1139c2d117c7c75abc1ad84c62d59c
Ryan Brown <ryan.brown@barre3.com>
2019-04-28 11:05:05 -0700

    send confirmation emails, add integration test

---
+++ test/mailers/order_mailer_test.rb
@@ -0,0 +1,19 @@
+class OrderMailerTest < ActionMailer::TestCase
+  test "received" do
+    mail = OrderMailer.received(orders(:one))
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    assert_match /Thank you for your recent order from The Pragmatic Store./, mail.body.encoded
+  end
+
+  test "shipped" do
+    mail = OrderMailer.shipped(orders(:one))
+    assert_equal "Pragmatic Store Order Shipped", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    assert_match /This is just to let you know that we've shipped your recent order/,
+      mail.body.encoded
+  end
+
+end
